371665551d1159a2a62600cdc60b101543026dd3
Eduardo Soares <edurbs@gmail.com>
2025-09-22 16:23:20 -0300

    first commit

---
+++ src/main/java/com/github/edurbs/datsa/auth/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.github.edurbs.datsa.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("eduardo")
+                .password(passwordEncoder().encode("123"))
+                .roles("Admin") // fake (for now)
+            .and()
+            .withUser("joao")
+                .password(passwordEncoder().encode("123"))
+                .roles("admin");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
+
