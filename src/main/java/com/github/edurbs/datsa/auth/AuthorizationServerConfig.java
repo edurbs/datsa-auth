dcecb27895b45b3a203dc16dbf625e1e1e5eae67
Eduardo Soares <edurbs@gmail.com>
2025-09-22 17:33:10 -0300

    add authorization server

---
+++ src/main/java/com/github/edurbs/datsa/auth/AuthorizationServerConfig.java
@@ -0,0 +1,37 @@
+package com.github.edurbs.datsa.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("datsa-web") // client identifier
+                .secret(passwordEncoder.encode("123")) // password
+                .authorizedGrantTypes("password") // authentication flow
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60*60*6); // 6 hours to token expiration
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager); // only the password flow needs this
+    }
+
+}
