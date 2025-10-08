8a4f8bed9961b5cd9f11f6404388f1c665f8d089
Eduardo Soares <edurbs@gmail.com>
2025-10-08 13:29:55 -0300

    add: auth clients from db

--- src/main/java/com/github/edurbs/datsa/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/github/edurbs/datsa/auth/core/AuthorizationServerConfig.java
@@ -3,13 +3,14 @@ package com.github.edurbs.datsa.auth.core;
 import java.security.KeyPair;
 import java.util.Arrays;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -34,9 +35,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private static final String WRITE = "WRITE";
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
@@ -45,43 +43,51 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private JwtKeyStoreProperties jwtKeyStoreProperties;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-            .inMemory()
-                .withClient("datsa-web") // client identifier
-                .secret(passwordEncoder.encode("123")) // password
-                .authorizedGrantTypes("password", "refresh_token") // password with refresh token flow
-                .scopes(WRITE, READ)
-                .accessTokenValiditySeconds(60*15) // 15 minutes to access token expiration
-                .refreshTokenValiditySeconds(60*60*12) // 12 hours refresh token
-            .and()
-                .withClient("other-backend") 
-                .secret(passwordEncoder.encode("123")) 
-                .authorizedGrantTypes("client_credentials") // client without user flow
-                .scopes(WRITE, READ)
-            .and()
-                .withClient("datsa-analytics") // client identifier
-                .secret(passwordEncoder.encode("")) // password
-                .authorizedGrantTypes("authorization_code") // authorization code flow with PKCE support
-                .scopes(WRITE, READ)
-                .redirectUris("http://localhost:8081") // in production must be https and use real client URL
-            .and()
-                .withClient("webadmin") // client identifier
-                .authorizedGrantTypes("implicit") // implicit authorization flow
-                .scopes(WRITE, READ)
-                .redirectUris("http://localhost") 
-            .and()
-                .withClient("checktoken")
-                .secret(passwordEncoder.encode("web123"));
+        clients.jdbc(dataSource);
+
+        // .inMemory()
+        // .withClient("datsa-web") // client identifier
+        // .secret(passwordEncoder.encode("123")) // password
+        // .authorizedGrantTypes("password", "refresh_token") // password with refresh
+        // token flow
+        // .scopes(WRITE, READ)
+        // .accessTokenValiditySeconds(60*15) // 15 minutes to access token expiration
+        // .refreshTokenValiditySeconds(60*60*12) // 12 hours refresh token
+        // .and()
+        // .withClient("other-backend")
+        // .secret(passwordEncoder.encode("123"))
+        // .authorizedGrantTypes("client_credentials") // client without user flow
+        // .scopes(WRITE, READ)
+        // .and()
+        // .withClient("datsa-analytics") // client identifier
+        // .secret(passwordEncoder.encode("")) // password
+        // .authorizedGrantTypes("authorization_code") // authorization code flow with
+        // PKCE support
+        // .scopes(WRITE, READ)
+        // .redirectUris("http://localhost:8081") // in production must be https and use
+        // real client URL
+        // .and()
+        // .withClient("webadmin") // client identifier
+        // .authorizedGrantTypes("implicit") // implicit authorization flow
+        // .scopes(WRITE, READ)
+        // .redirectUris("http://localhost")
+        // .and()
+        // .withClient("checktoken")
+        // .secret(passwordEncoder.encode("web123"));
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        //security.checkTokenAccess("isAuthenticated()"); // allow only with basic authentication
+        // security.checkTokenAccess("isAuthenticated()"); // allow only with basic
+        // authentication
         security.checkTokenAccess("permitAll()") // allow all without any authentication
-            .tokenKeyAccess("permitAll") // alow to get the public key
-            .allowFormAuthenticationForClients(); // allow client_id in body without basic auth.
+                .tokenKeyAccess("permitAll") // alow to get the public key
+                .allowFormAuthenticationForClients(); // allow client_id in body without basic auth.
     }
 
     @Override
@@ -89,26 +95,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         var enhancerChain = new TokenEnhancerChain();
         enhancerChain.setTokenEnhancers(Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
         endpoints
-            .authenticationManager(authenticationManager) // only the password flow needs this
-            .userDetailsService(userDetailsService)
-            .reuseRefreshTokens(false) // must not reuse refresh tokens
-            .accessTokenConverter(jwtAccessTokenConverter()) // generate JWT tokens (instead of opaque tokens)
-            .tokenEnhancer(enhancerChain) // add claims (custom properties) to JWT token
-            .approvalStore(approvalStore(endpoints.getTokenStore())) // must be later of accessTokenConverter
-            .tokenGranter(tokenGranter(endpoints));
+                .authenticationManager(authenticationManager) // only the password flow needs this
+                .userDetailsService(userDetailsService)
+                .reuseRefreshTokens(false) // must not reuse refresh tokens
+                .accessTokenConverter(jwtAccessTokenConverter()) // generate JWT tokens (instead of opaque tokens)
+                .tokenEnhancer(enhancerChain) // add claims (custom properties) to JWT token
+                .approvalStore(approvalStore(endpoints.getTokenStore())) // must be later of accessTokenConverter
+                .tokenGranter(tokenGranter(endpoints));
     }
 
-    private ApprovalStore approvalStore (TokenStore tokenStore){
+    private ApprovalStore approvalStore(TokenStore tokenStore) {
         var approvalStore = new TokenApprovalStore();
         approvalStore.setTokenStore(tokenStore);
         return approvalStore;
     }
 
     @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
         // command to generate the keystore
-        // keytool -genkeypair -alias datsa -keyalg RSA -keypass 123456 -keystore datsa.jks -storepass 123456 -validity 3650
+        // keytool -genkeypair -alias datsa -keyalg RSA -keypass 123456 -keystore
+        // datsa.jks -storepass 123456 -validity 3650
         ClassPathResource jksResource = new ClassPathResource(jwtKeyStoreProperties.getPath()); // keystore file
         String keyStorePass = jwtKeyStoreProperties.getPassword(); // password to open the keystore
         String keyPairAlias = jwtKeyStoreProperties.getKeypairAlias(); // key name
@@ -118,15 +125,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         return jwtAccessTokenConverter;
     }
 
-	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
-		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
-				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
-				endpoints.getOAuth2RequestFactory());
-		
-		var granters = Arrays.asList(
-				pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
-		
-		return new CompositeTokenGranter(granters);
-	}
+    private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+        var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+                endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+                endpoints.getOAuth2RequestFactory());
+
+        var granters = Arrays.asList(
+                pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+
+        return new CompositeTokenGranter(granters);
+    }
 
 }
