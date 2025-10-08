3df4fa0793217450db625a86c8e2cb292eaa7526
Eduardo Soares <edurbs@gmail.com>
2025-10-08 14:20:04 -0300

    add: auth user to tb

--- src/main/java/com/github/edurbs/datsa/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/github/edurbs/datsa/auth/core/AuthorizationServerConfig.java
@@ -49,6 +49,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.jdbc(dataSource);
+<<<<<<< HEAD
 
         // .inMemory()
         // .withClient("datsa-web") // client identifier
@@ -79,6 +80,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         // .and()
         // .withClient("checktoken")
         // .secret(passwordEncoder.encode("web123"));
+=======
+>>>>>>> 959811a (add: auth user to tb)
     }
 
     @Override
